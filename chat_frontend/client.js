3eb253f583423d6f6cc447c97b495a35169ae170
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-02-08 19:35:59 +0100

    client.js, index.html finished

--- chat_frontend/client.js
+++ chat_frontend/client.js
@@ -1,16 +1,70 @@
 var socket = io("http://localhost:5000/");
 
+let video = null;
+
+function stopStream() {
+  document.querySelector(".js-container").innerHTML = "";
+}
+
+function startStream(audioDeviceId) {
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: {
+        deviceId: audioDeviceId,
+      },
+      video: true,
+    })
+    .then((stream) => {
+      stopStream();
+      video = document.createElement("video");
+      video.setAttribute("autoplay", "true");
+      video.setAttribute("playsinline", "true");
+      video.setAttribute("controls", "true");
+      video.srcObject = stream;
+      document.querySelector(".js-container").appendChild(video);
+
+      window.stream = stream;
+    });
+}
+
+// navigator.mediaDevices
+//   .getUserMedia({
+//     audio: true,
+//     video: true,
+//   })
+//   .then((stream) => {
+//     const video = document.createElement("video");
+//     video.setAttribute("autoplay", "true");
+//     video.setAttribute("playsinline", "true");
+//     video.setAttribute("controls", "true");
+//     video.srcObject = stream;
+//     document.querySelector(".js-container").appendChild(video);
+//   });
+
 navigator.mediaDevices
-  .getUserMedia({
-    audio: true,
-    video: true,
+  .enumerateDevices()
+  .then((devices) => {
+    let html = "";
+    [...devices].map(({ kind, label, deviceId }) => {
+      if (kind === "audioinput") {
+        html += `<option value="${deviceId}">${label}</option>`;
+      }
+    });
+    const devicesSelect = document.querySelector(".js-input-devices");
+    devicesSelect.innerHTML = html;
+    devicesSelect.addEventListener("change", changeMicrophone);
   })
-  .then((stream) => {
-    const video = document.createElement("video");
-    video.setAttribute("autoplay", "true");
-    video.setAttribute("playsinline", "true");
-    video.setAttribute("controls", "true");
-    video.srcObject = stream;
-    document.querySelector(".js-container").appendChild(video);
+  .catch(function (err) {
+    console.log(err.name + ": " + err.message);
   });
 
+function changeMicrophone(event) {
+  console.log(event.target.value);
+}
+
+document.querySelector(".js-start-stream").addEventListener("click", () => {
+  const microphoneId = document.querySelector(".js-input-devices").value;
+  startStream(microphoneId);
+});
+
+document.querySelector(".js-stop-stream").addEventListener("click", stopStream);
